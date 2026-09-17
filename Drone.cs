f2d080bd52c842b06cb7b4a84458c27a7c9affab
43947679890 <43947679890@ECE337N25SN790>
2026-09-17 09:01:21 -0300

    InterfacesClassesAbstratas

---
+++ Drone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesClassesAbstratas
+{
+    internal class Drone : ICamera, IVoavel
+    {
+        public void TirarFoto()
+        {
+            Console.WriteLine("Tirando foto panorâmica...");
+        }
+        public void Decolar()
+        {
+            Console.WriteLine("Drone decolando verticalmente...");
+        }
+        public void Pousar()
+        {
+            Console.WriteLine("Drone pousando...");
+        }
+    }
+}
